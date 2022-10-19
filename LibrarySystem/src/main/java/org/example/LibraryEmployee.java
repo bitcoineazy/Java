790bf81092a2ed8471a4fb3e293415881d85826d
Nikita <nakoryakin02@gmail.com>
2022-10-19 20:33:56 +0300

    Поиск для библиотечного работника

--- LibrarySystem/src/main/java/org/example/LibraryEmployee.java
+++ LibrarySystem/src/main/java/org/example/LibraryEmployee.java
@@ -3,6 +3,9 @@ package org.example;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LibraryEmployee extends Employee {
 
@@ -13,6 +16,56 @@ public class LibraryEmployee extends Employee {
     public LibraryEmployee(int id, String name, String surname, String middleName, String address, int employeeId) {
         super(id, name, surname, middleName, address, employeeId);
         readLog();
+    }
+
+    public void giveBookFromOption(Book books, Reader reader, HashMap<String, String> bookOptions, ArrayList<Integer> IDGetBooks){
+        ArrayList<Integer> indexes_old = new ArrayList<>();
+        ArrayList<Integer> indexes_new = new ArrayList<>();
+        ArrayList<Integer> indexes = new ArrayList<>();
+        int count = 0;
+        for (Map.Entry entry: bookOptions.entrySet()) {
+            indexes.clear();
+            indexes_new = books.getIndexesFromValue((String) entry.getKey(), (String) entry.getValue());
+            if (count == 0){
+                indexes_old.addAll(indexes_new);
+            }
+            for (Integer index :indexes_new) {
+                if (indexes_old.contains(index)){
+                    indexes.add(index);
+                }
+            }
+            indexes_old.clear();
+            indexes_old.addAll(indexes);
+            count++;
+
+        }
+        ArrayList<Integer> IdsBook = new ArrayList<>();
+        for (Integer index :indexes_old) {
+            try {
+                IdsBook.add(Integer.valueOf(books.getRowFromIndex(index).get("id")));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        for(Integer id: IdsBook){
+            if (IDGetBooks.contains(id)){
+                if (reader.bookIds.contains(id)) {
+                    System.out.println("У читателя уже есть эта книга");
+                    return;
+                } else {
+                    continue;
+                }
+            }
+            reader.bookAmount += 1;
+            reader.bookIds.add(id);
+            System.out.println("Читателю выдана книга с ID: " + id);
+            log(String.format("Reader id %s, name %s, surname %s, middleName %s, address %s; get book with id %s",
+                    reader.id, reader.name, reader.surname, reader.middleName, reader.address, id));
+
+        }
+        System.out.println("Все искомые книги уже взяты");
+
     }
 
     public void giveBookToReader(Book books, Reader reader, int id) throws Exception {
